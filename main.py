e41d0c8db12184d52661d757d0f3502924daae19
Barış Can Yılmaz <30300440+bariscanyilmaz@users.noreply.github.com>
2024-10-04 00:12:09 +0300

    scrape english data with cleaned content

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 import requests
 import json
 from pathlib import Path
@@ -9,19 +10,22 @@ class Month:
         self.name=name
         self.day_count=day_count
 
-months=[Month("Ocak",31),Month("Şubat",29) ,Month("Mart",31),Month("Nisan",30),Month("Mayıs",31),Month("Haziran",30),Month("Temmuz",31),Month("Ağustos",31),Month("Eylül",30),Month("Ekim",31),Month("Kasım",30),Month("Aralık",31)]
+tr_months=[Month("Ocak",31),Month("Şubat",29) ,Month("Mart",31),Month("Nisan",30),Month("Mayıs",31),Month("Haziran",30),Month("Temmuz",31),Month("Ağustos",31),Month("Eylül",30),Month("Ekim",31),Month("Kasım",30),Month("Aralık",31)]
+months=[Month("January",31),Month("February",29) ,Month("March",31),Month("April",30),Month("May",31),Month("June",30),Month("July",31),Month("August",31),Month("September",30),Month("October",31),Month("November",30),Month("December",31)]
+pattern = r"\[\d+\]"
 
 for month_idx in range(len(months)):
     month=months[month_idx]
     for i in range(1,month.day_count+1):
-        url="https://tr.wikipedia.org/wiki/{}_{}".format(i,month.name)
+        url="https://en.wikipedia.org/wiki/{}_{}".format(month.name,i)
         response = requests.get(url)
         soup = BeautifulSoup(response.content.decode('utf-8','ignore'), 'html.parser')
-        eventsList = [str(child.get_text()) for child in soup.find("ul",{"class":""}).children if child.get_text()!='\n']
+        events_ul_list=soup.find_all("ul",{"class":""})[:3]
+        eventsList = [str( re.sub(pattern, "", child.get_text())) for ul in map(lambda ul:ul,events_ul_list) for child in ul.children if child.get_text()!='\n']
         eventsList.reverse()
         json_event_list=json.dumps(eventsList,ensure_ascii=False)
-        Path(str(month_idx)).mkdir(parents=True, exist_ok=True)
-        file_name = str(month_idx)+"/"+str(i)+".json"
+        Path("en/"+str(month_idx)).mkdir(parents=True, exist_ok=True)
+        file_name ="en/"+str(month_idx)+"/"+str(i)+".json"
         f = open(file_name, 'w+',encoding='utf-8')  # open file in write mode
         f.write(json_event_list)
         f.close()
